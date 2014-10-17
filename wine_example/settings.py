3dbeae708bde030d968e84750500fbdb86933e89
Matthew L Daniel <mdaniel@radius.com>
2014-10-17 16:11:40 -0700

    Turn on nice settings for class

--- wine_example/settings.py
+++ wine_example/settings.py
@@ -1,6 +1,18 @@
 # this is the name of the package, not a list of spiders
 SPIDER_MODULES = ['wine_example.spiders']
+# indicates where to put newly generated spiders
 NEWSPIDER_MODULE = SPIDER_MODULES[0]
 COOKIES_ENABLED = False
+
+# BEGIN: slow down request settings for class
+CONCURRENT_REQUESTS = 1
+CONCURRENT_REQUESTS_PER_DOMAIN = 1
+# CONCURRENT_REQUESTS_PER_IP = 1
+DOWNLOAD_DELAY = 1.0  # seconds
+# END: slow down request settings for class
+
+DUPEFILTER_DEBUG = True
+
+HTTPCACHE_ENABLED = True
 
 USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:33.0) Gecko/20100101 Firefox/33.0'
